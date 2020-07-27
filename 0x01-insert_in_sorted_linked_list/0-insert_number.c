0432e0b2d4bd0b73486290a2fa65bcccceb301fc
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-27 03:57:33 +0000

    Uploading task 0 c file with function to insert node in addition to clean dirs, header, and c file

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+
+/**
+ * insert_node - inserts node into linked list
+ * @head: pointer to first pointer in array
+ * @number: value to be inserted
+ * Return: pointer to inserted node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *curr;
+	listint_t *save;
+
+	if ((head == NULL) || (*head == NULL))
+		return (NULL);
+
+	curr = *head;
+	save = NULL;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+	while (number > curr->next->n)
+	{
+		curr = curr->next;
+	}
+	save = curr->next;
+	curr->next = new;
+	new->next = save;
+
+	return (new);
+}
