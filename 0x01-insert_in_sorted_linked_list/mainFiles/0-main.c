0e332781f8a6d106940318b768faa7210dacbbce
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-27 04:57:14 +0000

    Updating clean dirs

--- 0x01-insert_in_sorted_linked_list/mainFiles/0-main.c
+++ 0x01-insert_in_sorted_linked_list/mainFiles/0-main.c
@@ -13,21 +13,29 @@ int main(void)
 	listint_t *head;
 
 	head = NULL;
-	add_nodeint_end(&head, 0);
+	insert_node(&head, 0);
+	insert_node(&head, 10);
+	insert_node(&head, 2);
+	insert_node(&head, -3);
+	insert_node(&head, 8);
+/**	add_nodeint_end(&head, 0);
 	add_nodeint_end(&head, 1);
 	add_nodeint_end(&head, 2);
 	add_nodeint_end(&head, 3);
 	add_nodeint_end(&head, 4);
 	add_nodeint_end(&head, 98);
 	add_nodeint_end(&head, 402);
-	add_nodeint_end(&head, 1024);
+	add_nodeint_end(&head, 1024);*/
 	print_listint(head);
 
 	printf("-----------------\n");
 
-	insert_node(&head, 27);
+/**	insert_node(&head, 27);
 	insert_node(&head, -1);
-	insert_node(&head, 5000);
+	insert_node(&head, 5000);*/
+
+	insert_node(&head, 1);
+	insert_node(&head, -1);
 
 	print_listint(head);
 
