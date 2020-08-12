e9593ff98c4729a91a0196d8efa9baf591ad5c7e
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-08-12 04:53:29 +0000

    Checking for type of n

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,19 +4,19 @@
 
 def minOperations(n):
     """determines min operations to generate n copied output"""
-    if n == 1:
-        return n
-    elif n <= 0:
+    if notisinstance(n, int) or n <= 0:
         return 0
+    elif n == 1:
+        return 1
     opCount = 2
-    start = "HH"
-    clipB = "H"
-    while len(start) < n:
-        if n % len(start) == 0:
-            clipB = start
-            start += clipB
+    textLen = len("HH")
+    clipB = len("H")
+    while textLen < n:
+        if n % textLen == 0:
+            clipB = textLen
+            textLen += clipB
             opCount += 2
         else:
-            start += clipB
+            textLen += clipB
             opCount += 1
     return opCount
